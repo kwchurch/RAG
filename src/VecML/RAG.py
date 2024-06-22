bd04285962c60f1e3ec916b4cded7b205b5765eb
kwchurch <kenneth.ward.church@gmail.com>
2024-06-21 20:15:17 -0700

    timing

--- src/VecML/RAG.py
+++ src/VecML/RAG.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
 from vecml import vecml
-import sys,os
+import sys,os,time
 
 chatName = sys.argv[1]
 
 vecml.init(os.environ['VECML_API_KEY'], "us-west")
 vecml.create_chat(chatName, sys.argv[1:])
 
+t0 = time.time()
 for prompt in sys.stdin:
     print(vecml.chat(chatName, prompt))
 
 vecml.delete_data(chatName)
+print(str(time.time() - t0) + ' seconds')
 
